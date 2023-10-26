e04c74a78a1c6b3878eba342d6d33fed50597131
Yash Narwal <ynarwal10@gmail.com>
2023-10-26 12:35:47 +1100

    add activities  api

---
+++ API/Controllers/ActivityController.cs
@@ -0,0 +1,31 @@
+using Domain;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers
+{
+    public class ActivitiesController : BaseApiController
+    {
+        private readonly DataContext context;
+        public ActivitiesController(DataContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> GetActivities()
+        {
+            return await this.context.Activities.ToListAsync();
+        }
+
+        // TODO: it's not returning 404
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> GetActivity(Guid id)
+        {
+            var activity = await this.context.Activities.FindAsync(id);
+            return activity == null ? NotFound() : activity;
+        }
+    }
+}
